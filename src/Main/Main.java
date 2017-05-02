74112359842fb757368f153bed165523075c4953
Pedro Neves Alvarez <pedronalvarez@Pedros-MacBook-Pro.local>
2017-05-01 22:32:06 -0300

    Adding diagrams

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,8 +4,13 @@ import View.WashingFrame;
 
     class Main {
 
+    private static WashingFrame frame;
+    
 	public static void main(String[] args){
 		
-         new WashingFrame();
+         initializeFrame(frame);
+	}
+	static void initializeFrame(WashingFrame fr){
+		fr = new WashingFrame();
 	}
 }
